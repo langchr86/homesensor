e6849090b7c692f857500e28c94cab1238c18fb5
Christian Lang <lang.chr86@gmail.com>
2021-01-10 20:54:06 +0100

    * increase max battery voltage because the charger goes to 4.2V

--- software/src/sensors/battery_calculation.h
+++ software/src/sensors/battery_calculation.h
@@ -5,5 +5,5 @@
 class BatteryCalculation
 {
 public:
-    static float CapacityLevelPercent(float voltage, float low_voltage = 3.3, float high_voltage = 4.1);
+    static float CapacityLevelPercent(float voltage, float low_voltage = 3.3, float high_voltage = 4.2);
 };
